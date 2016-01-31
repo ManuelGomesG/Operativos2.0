1e839840945478b6590c6d37321a6699b18682fe
francisco <f.rojaskey@gmail.com>
2016-01-30 21:52:19 -0430

    lista de penes

--- lista.c
+++ lista.c
@@ -1,122 +1,127 @@
 #include "lista.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-void CrearLista(identificador *id){
-	id = (identificador *)malloc(sizeof(identificador));
-	id-> inicio = NULL;
-	id-> fin = NULL;
-	id-> tam = 0; 
+identificador* CrearListaU(identificador *id){
+    id = (identificador *)malloc(sizeof(identificador));
+    id-> inicio = NULL;
+    id-> fin = NULL;
+    id-> tam = 0; 
+    return id;
 }
 
-void Agregar(identificador *id, char *nombre){
+void AgregarUsuario(identificador *id, char *nombre){
+    lista *temp;
+    temp = (lista *)malloc(sizeof(lista));
+    temp->nombre = (char *)malloc(sizeof(nombre));
+    temp->nombre = nombre;
+    temp->next = NULL;
 
-	lista *temp;
-	temp = (lista *)malloc(sizeof(lista));
-	temp->nombre = (char *)malloc(sizeof(nombre));
-	temp->next = NULL;
-
-	if(id->tam == 0){
-		id->inicio = temp;
-		id->fin = temp;
+    if(id->tam == 0){
+        id->inicio = (lista *)malloc(sizeof(lista));
+        id->inicio = temp;
+        id->fin = temp;
 	}
-	else{
-		temp->next = id->inicio;
-		id->inicio = temp;
+    else{
+        temp->next = (lista *)malloc(sizeof(lista));
+        temp->next = id->inicio;
+        id->inicio = temp;
 	}
 
-	id->tam++;
+    id->tam++;
 }
 
-void Imprimir(identificador *id){
-	lista *temp;
-	temp = (lista *)malloc(sizeof(lista));
-	temp = id->inicio;
-	int i = 1;
-	while(i <= id->tam){
-		printf("soy el elemento %d y mi dato es %s \n", i, temp->nombre);
-		temp=temp->next;
-		i++;
-	}
-}
-
-void Eliminar(identificador *id, char *nombre){
-	lista *temp;
-	temp = (lista *)malloc(sizeof(lista));
-	temp = id->inicio;
-	lista *temp_ant;
-	temp_ant = (lista *)malloc(sizeof(lista));
-	int i = 1;
-	while(i <= id->tam ){
-		if(temp->nombre == d){
-			if(i == 1 && i != id->tam ){
-				id->inicio= temp->next;
-			}
-			else if(i == id->tam && i == 1){
-				id->inicio = NULL;
-				id->fin = NULL;
-			}
-			else if(i == id->tam && i > 1){
-				id->fin = temp_ant;
-			}
-			else{
-				temp_ant->next = temp->next;
-			}
-			id->tam--;
-		}
-		i++;
-		temp_ant = temp;
-		temp = temp->next;
+void ImprimirUsuarios(identificador *id){
+    lista *temp;
+    temp = (lista *)malloc(sizeof(lista));
+    temp = id->inicio;
+    int i = 1;
+    while(i <= id->tam){
+        printf("soy el elemento %d y mi dato es %s \n", i, temp->nombre);
+        temp=temp->next;
+        i++;
 	}
 }
 
-void CrearLista(identificador_chat *id){
-	id = (identificador_chat *)malloc(sizeof(identificador_chat));
-	id-> inicio = NULL;
-	id-> fin = NULL;
-	id-> tam = 0; 
+void EliminarUsuario(identificador *id, char *nombre){
+    lista *temp;
+    temp = (lista *)malloc(sizeof(lista));
+    temp = id->inicio;
+    lista *temp_ant;
+    temp_ant = (lista *)malloc(sizeof(lista));
+    int i = 1;
+    while(i <= id->tam ){
+        if(temp->nombre == nombre){
+            if(i == 1 && i != id->tam ){
+                id->inicio= temp->next;
+            }
+            else if(i == id->tam && i == 1){
+                id->inicio = NULL;
+                id->fin = NULL;
+            }
+            else if(i == id->tam && i > 1){
+                id->fin = temp_ant;
+            }
+            else{
+                temp_ant->next = temp->next;
+            }
+            id->tam--;
+        }
+        i++;
+        temp_ant = temp;
+        temp = temp->next;
+	}
 }
 
-void Agregar(identificador_chat *id, char *nombre){
+identificador_chat* CrearListaChat(identificador_chat *id){
+    id = (identificador_chat *)malloc(sizeof(identificador_chat));
+    id-> inicio = NULL;
+    id-> fin = NULL;
+    id-> tam = 0; 
+    return id;
+}
 
-	lista *temp;
+void AgregarChat(identificador_chat *id, char *nombre){
+
+    lista_chat *temp;
+    temp = (lista_chat *)malloc(sizeof(lista_chat));
+    temp->nombre = (char *)malloc(sizeof(nombre));
+    temp->next = NULL;
+
+    if(id->tam == 0){
+        id->inicio = temp;
+        id->fin = temp;
+    }
+    else{
+        temp->next = id->inicio;
+        id->inicio = temp;
+    }
+
+    id->tam++;
+}
+
+void ImprimirChat(identificador_chat *id){
+    lista_chat *temp;
+    temp = (lista_chat *)malloc(sizeof(lista_chat));
+    temp = id->inicio;
+    int i = 1;
+    while(i <= id->tam){
+        printf("soy el elemento %d y mi dato es %s \n", i, temp->nombre);
+    temp=temp->next;
+        i++;
+    }
+}
+
+void EliminarChat(identificador_chat *id, char *nombre){
+	lista_chat *temp;
 	temp = (lista_chat *)malloc(sizeof(lista_chat));
-	temp->nombre = (char *)malloc(sizeof(nombre));
-	temp->next = NULL;
-
-	if(id->tam == 0){
-		id->inicio = temp;
-		id->fin = temp;
-	}
-	else{
-		temp->next = id->inicio;
-		id->inicio = temp;
-	}
-
-	id->tam++;
-}
-
-void Imprimir(identificador_chat *id){
-	lista *temp;
-	temp = (lista_chat *)malloc(sizeof(lista_chat));
 	temp = id->inicio;
-	int i = 1;
-	while(i <= id->tam){
-		printf("soy el elemento %d y mi dato es %s \n", i, temp->nombre);
-		temp=temp->next;
-		i++;
-	}
-}
-
-void Eliminar(identificador_chat *id, int d){
-	lista *temp;
-	temp = (lista_chat *)malloc(sizeof(lista_chat));
-	temp = id->inicio;
-	lista *temp_ant;
+	lista_chat *temp_ant;
 	temp_ant = (lista_chat *)malloc(sizeof(lista_chat));
 	int i = 1;
 	while(i <= id->tam ){
-		if(temp->nombre == d){
+		if(temp->nombre == nombre){
 			if(i == 1 && i != id->tam ){
 				id->inicio= temp->next;
 			}
@@ -136,4 +141,20 @@ void Eliminar(identificador_chat *id, int d){
 		temp_ant = temp;
 		temp = temp->next;
 	}
+}
+
+int main(){
+    identificador *id;
+    id = CrearListaU(id);
+    char *u1 = "francisco";
+    char *u2 = "manuel";
+    AgregarUsuario(id, u1);
+    AgregarUsuario(id, u2);
+    ImprimirUsuarios(id);
+    EliminarUsuario(id,u1);
+    ImprimirUsuarios(id);
+    getchar();
+    
+
+    return 0;
 }
--- lista.h
+++ lista.h
@@ -1,35 +1,36 @@
-
 typedef struct Lista{
     char *nombre;
     char *estado;
     struct Identificador_Chat *chat;
     int fd_read;
     int fd_write;
-	int data;
-	struct Lista *next;
+    struct Lista *next;
 }lista;
 
 typedef struct Lista_Chat{
     char *nombre;
-    struct Lista_chat *next;
+    struct Lista_Chat *next;
 
 }lista_chat;
 
-typedef struct Identificador
-{
-	struct Lista *inicio;
-	struct Lista *fin;
-	int tam;
+typedef struct Identificador{
+    struct Lista *inicio;
+    struct Lista *fin;
+    int tam;
 }identificador;
 
-typedef struct Identificador_Chat
-{
-	struct Lista_Chat *inicio;
-	struct Lista_Chat *fin;
-	int tam;
+typedef struct Identificador_Chat{
+    struct Lista_Chat *inicio;
+    struct Lista_Chat *fin;
+    int tam;
 }identificador_chat;
 
-void CrearLista(identificador *);
-void Agregar(identificador * , int );
-void Imprimir(identificador *);
-void Eliminar(identificador *, int);
+
+identificador * CrearListaU(identificador *);
+void AgregarUsuario(identificador * , char *);
+void ImprimirUsuarios(identificador *);
+void EliminarUsuario(identificador *, char *);
+identificador_chat * CrearListaChat(identificador_chat *);
+void AgregarChat(identificador_chat * , char * );
+void ImprimirChat(identificador_chat *);
+void EliminarChat(identificador_chat *, char *);
